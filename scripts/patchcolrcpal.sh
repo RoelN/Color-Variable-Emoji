dfa2c395312c93bcbed938a7ea7566a379e3c833
Roel Nieskens <roel@pixelambacht.nl>
2017-01-30 10:34:42 +0100

    User proper ttx oneliner to generate COLR/CPAL font

--- scripts/patchcolrcpal.sh
+++ scripts/patchcolrcpal.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 # Add COLR/CPAL table
-ttx -m fonts/ColorVariableEmoji-VF.ttf scripts/colr-cpal-tables.ttx
-# Can't seem to set output path+filename, so move the result
-mv scripts/colr-cpal-tables.ttf fonts/ColorVariableEmoji-VF-colr.ttf
+ttx -o fonts/ColorVariableEmoji-VF-colr.ttf -m fonts/ColorVariableEmoji-VF.ttf scripts/colr-cpal-tables.ttx

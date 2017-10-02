773ade0a141269f86225f31764d1ff177fe085f4
Roel Nieskens <roel@pixelambacht.nl>
2017-10-02 20:50:14 +0200

    Added demo that works on W10 Creators Update

--- scripts/patchcolrcpal.sh
+++ scripts/patchcolrcpal.sh
@@ -2,3 +2,4 @@
 
 # Add COLR/CPAL table
 ttx -o fonts/ColorVariableEmoji-VF-colr.ttf -m fonts/ColorVariableEmoji-VF.ttf scripts/colr-cpal-tables.ttx
+ttx -o sources/1-drawing/ColorVariableEmoji-VF-colr.ttf -m sources/1-drawing/ColorVariableEmoji-VF.ttf scripts/colr-cpal-tables.ttx
